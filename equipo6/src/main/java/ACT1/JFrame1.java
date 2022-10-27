4a2ec5b2acf0583f10d0eeadfa1f0f87df25f634
Dani Martos <dani.martos69@gmail.com>
2022-10-27 12:14:06 +0200

    ej1

---
+++ equipo6/src/main/java/ACT1/JFrame1.java
@@ -0,0 +1,59 @@
+package ACT1;
+
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class JFrame1 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame1 frame = new JFrame1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrame1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("JFrame1");
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel etiqueta= new JLabel("Etiqueta");
+		etiqueta.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(arg0.getClickCount()==2) {
+					etiqueta.resize(getPreferredSize());
+				}
+			}
+		});
+		etiqueta.setAlignmentX(Component.CENTER_ALIGNMENT);
+		etiqueta.setBounds(121, 35, 151, 14);
+		contentPane.add(etiqueta);
+	}
+
+}
